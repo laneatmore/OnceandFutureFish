7509783160031cac3df8a983b9e2911a828280f0
laneatmore <52425233+laneatmore@users.noreply.github.com>
2024-12-10 10:47:58 -0800

    Create Submit_paleomix_ancient.sh

---
+++ Alignment/Submit_paleomix_ancient.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+module restore R_paleomix
+
+#then load the dependencies
+module load Python/3.10.4-GCCcore-11.3.0
+module load AdapterRemoval/2.3.3-GCC-11.3.0
+module load BWA/0.7.17-GCCcore-11.3.0
+module load SAMtools/1.16.1-GCC-11.3.0
+
+#activate the paleomix environment
+source ~/paleomix/bin/activate
+
+echo "Paleomix env activated"
+
+#create individual makefiles
+cat core_herring_make_ancient.yaml $1 > core_yaml_${1}
+
+echo "Running paleomix on ${1}"
+
+#make sure you have used bwa to index your reference genome before running
+#there seems to be a bug if you do it through paleomix
+
+#run Paleomix
+paleomix bam run core_yaml_${1} \
+--max-threads 5 \
+--bwa-max-threads 4 
+
+echo "Paleomix run is done"
